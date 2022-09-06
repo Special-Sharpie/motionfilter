f2fb14648d798878496c83b0adfbd46154e80cc1
BuildTools <daniel.palmer@shaw.ca>
2022-09-05 22:09:59 -0600

    add motion filter

--- motionfilter.lua
+++ motionfilter.lua
@@ -1,5 +1,19 @@
-local x = 1
+while true
+do
 
-if x == 1 then
-    print("Hello World!")
+eventName, addr, x, y, z, entityName = event.pull("motion")
+
+if (entityName == "special_sharpie")
+then
+component.redstone.setOutput(2, 15)
+print(eventName, entityName)
+os.execute("sleep " .. tonumber(3))
+
+component.redstone.setOutput(2, 0)
+component.redstone.setOutput(2, 15)
+component.redstone.setOutput(2, 0)
+else
+print("Access Not Permitted")
+end
+
 end
