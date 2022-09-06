7f7a4015ba7e3c69ed7339ef5056fafb7bb09a08
BuildTools <daniel.palmer@shaw.ca>
2022-09-06 10:10:22 -0600

    add table check

--- motionfilter.lua
+++ motionfilter.lua
@@ -1,19 +1,34 @@
+names = {"special_sharpie", "CompulsiveGaming"}
+
+local function inTable(table, value)
+    for index, tableValue in ipairs(table)
+    do
+        if value == tableValue
+        then
+            return true
+        else
+            return false
+        end
+    end
+end
+
 while true
 do
 
 eventName, addr, x, y, z, entityName = event.pull("motion")
 
-if (entityName == "special_sharpie")
+if (inTable(names, entityName))
 then
-component.redstone.setOutput(2, 15)
-print(eventName, entityName)
-os.execute("sleep " .. tonumber(3))
+    component.redstone.setOutput(2, 15)
+    print(eventName, entityName)
+    os.execute("sleep " .. tonumber(3))
 
-component.redstone.setOutput(2, 0)
-component.redstone.setOutput(2, 15)
-component.redstone.setOutput(2, 0)
+    component.redstone.setOutput(2, 0)
+    component.redstone.setOutput(2, 15)
+    component.redstone.setOutput(2, 0)
 else
-print("Access Not Permitted")
+    print("Access Not Permitted")
 end
 
 end
+
